43c325e377aabb4f9aae5f5d105e319e5cf69238
slobo10 <97814567+slobo10@users.noreply.github.com>
2023-12-11 14:41:05 -0800

    Setup environment

---
+++ App.tsx
@@ -0,0 +1,23 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const App: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Game's not ready yet!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
