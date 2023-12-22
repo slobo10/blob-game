4188d644cbd5a9022bf93de8e1cdb356c7ec34ad
slobo10 <97814567+slobo10@users.noreply.github.com>
2023-12-21 20:33:44 -0800

    Add eating of blobs

--- constants/types.ts
+++ constants/types.ts
@@ -2,12 +2,14 @@ type blobType = {
   position: [number, number];
   size: number;
   color: [number, number, number];
+  id: string;
   playerControlled?: boolean;
 };
 
 type GameContextType = {
   gameSvgDimensions: [number, number];
   blobs: blobType[];
+  changeNumberOfBlobs: Function;
   blobSpeed: number;
   keyDownEventHandlers: ((key: string) => void)[];
   keyUpEventHandlers: ((key: string) => void)[];
--- screens/Game.tsx
+++ screens/Game.tsx
@@ -2,31 +2,47 @@ import React, {
   Context,
   createContext,
   useEffect,
-  useRef
+  useRef,
+  useState,
 } from "react";
 import { Svg } from "react-native-svg";
-import { GameContextType } from "../constants/types";
+import { GameContextType, blobType } from "../constants/types";
 import styles from "../constants/styles";
 import Blob from "../components/Blob";
+import { randomColor } from "../lib/mathLib";
 
 let GameContext: Context<GameContextType | undefined> =
   createContext(undefined);
 
 const Game: React.FC = () => {
+  let [blobs, setBlobs]: [blobType[], Function] = useState([
+    {
+      position: [175, 250],
+      size: 50,
+      color: randomColor(),
+      id: "playerBlob",
+      playerControlled: true,
+    },
+    {
+      position: [325, 250],
+      size: 60,
+      color: randomColor(),
+      id: "unmannedBlob0",
+    },
+    {
+      position: [325, 150],
+      size: 40,
+      color: randomColor(),
+      id: "unmannedBlob1",
+    },
+  ]);
+
   let GameContextValue: { current: GameContextType } = useRef({
     gameSvgDimensions: [500, 500],
-    blobs: [
-      {
-        position: [250, 250],
-        size: 50,
-        color: [
-          Math.floor(Math.random() * 255),
-          Math.floor(Math.random() * 255),
-          Math.floor(Math.random() * 255),
-        ],
-        playerControlled: true
-      },
-    ],
+    blobs: [...blobs],
+    changeNumberOfBlobs: () => {
+      setBlobs([...GameContextValue.current.blobs]);
+    },
     blobSpeed: 10000,
     keyDownEventHandlers: [],
     keyUpEventHandlers: [],
@@ -37,6 +53,7 @@ const Game: React.FC = () => {
   let blobOutput: React.JSX.Element[] = [];
   let i: number;
 
+  //TODO: Do the key event and such
   useEffect(() => {
     document.addEventListener("keydown", ({ key }: { key: string }) => {
       let i: number;
@@ -58,6 +75,57 @@ const Game: React.FC = () => {
       }
     });
 
+    GameContextValue.current.updateFunctions.push(() => {
+      let i: number;
+      let j: number;
+      let FirstBlobIsBigger: boolean;
+
+      for (i = 0; i < GameContextValue.current.blobs.length; i++) {
+        for (j = i; j < GameContextValue.current.blobs.length; j++) {
+          FirstBlobIsBigger =
+            GameContextValue.current.blobs[i].size >
+            GameContextValue.current.blobs[j].size;
+
+          if (i === j) {
+            continue;
+          }
+
+          if (
+            Math.sqrt(
+              (GameContextValue.current.blobs[i].position[0] -
+                GameContextValue.current.blobs[j].position[0]) **
+                2 +
+                (GameContextValue.current.blobs[i].position[1] -
+                  GameContextValue.current.blobs[j].position[1]) **
+                  2
+            ) <=
+            (FirstBlobIsBigger
+              ? GameContextValue.current.blobs[i].size -
+                GameContextValue.current.blobs[j].size
+              : GameContextValue.current.blobs[j].size -
+                GameContextValue.current.blobs[i].size)
+          ) {
+            if (FirstBlobIsBigger) {
+              GameContextValue.current.blobs[i].size = Math.sqrt(
+                GameContextValue.current.blobs[i].size ** 2 +
+                  GameContextValue.current.blobs[j].size ** 2
+              );
+              GameContextValue.current.blobs.splice(j, 1);
+              j--;
+            } else {
+              GameContextValue.current.blobs[j].size = Math.sqrt(
+                GameContextValue.current.blobs[j].size ** 2 +
+                  GameContextValue.current.blobs[i].size ** 2
+              );
+              GameContextValue.current.blobs.splice(i, 1);
+              i--;
+            }
+            GameContextValue.current.changeNumberOfBlobs();
+          }
+        }
+      }
+    });
+
     setInterval(() => {
       let i: number;
 
@@ -68,7 +136,7 @@ const Game: React.FC = () => {
   }, []);
 
   for (i = 0; i < GameContextValue.current.blobs.length; i++) {
-    blobOutput.push(<Blob id={i} />);
+    blobOutput.push(<Blob key={GameContextValue.current.blobs[i].id} id={i} />);
   }
 
   console.log("Game rendered!");
