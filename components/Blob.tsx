267b485a28a44e3d74748cf54365545c2ec66428
slobo10 <97814567+slobo10@users.noreply.github.com>
2024-01-12 14:16:00 -0800

    Move a realy long calculation to mathlib.ts

--- components/Blob.tsx
+++ components/Blob.tsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useRef, useState } from "react";
 import { Circle } from "react-native-svg";
 import { GameContextType, blobType } from "../constants/types";
 import { GameContext } from "../screens/Game";
+import { RGBtoString } from "../lib/mathLib";
 
 const Blob: React.FC<{ id: number }> = ({ id }) => {
   let [thisBlob, setThisBlob]: [blobType, Function] = useState({
@@ -15,9 +16,6 @@ const Blob: React.FC<{ id: number }> = ({ id }) => {
   );
   let xSpeed: { current: number } = useRef(0);
   let ySpeed: { current: number } = useRef(0);
-
-  let fillColor: string = "#";
-  let i: number;
 
   useEffect(() => {
     if (thisBlob.playerControlled) {
@@ -94,74 +92,7 @@ const Blob: React.FC<{ id: number }> = ({ id }) => {
     }, [thisBlob.position]);
   }
 
-  for (i = 0; i < GameContextValue.current.blobs[id].color.length; i++) {
-    let colorCharacterNumber: number = Math.floor(
-      GameContextValue.current.blobs[id].color[i] / 16
-    );
-
-    switch (colorCharacterNumber) {
-      case 10: {
-        fillColor += "a";
-        break;
-      }
-      case 11: {
-        fillColor += "b";
-        break;
-      }
-      case 12: {
-        fillColor += "c";
-        break;
-      }
-      case 13: {
-        fillColor += "d";
-        break;
-      }
-      case 14: {
-        fillColor += "e";
-        break;
-      }
-      case 15: {
-        fillColor += "f";
-        break;
-      }
-      default: {
-        fillColor += colorCharacterNumber.toString();
-      }
-    }
-
-    colorCharacterNumber =
-      GameContextValue.current.blobs[id].color[i] - colorCharacterNumber * 16;
-
-    switch (colorCharacterNumber) {
-      case 10: {
-        fillColor += "a";
-        break;
-      }
-      case 11: {
-        fillColor += "b";
-        break;
-      }
-      case 12: {
-        fillColor += "c";
-        break;
-      }
-      case 13: {
-        fillColor += "d";
-        break;
-      }
-      case 14: {
-        fillColor += "e";
-        break;
-      }
-      case 15: {
-        fillColor += "f";
-        break;
-      }
-      default: {
-        fillColor += colorCharacterNumber.toString();
-      }
-    }
-  }
+  const fillColor: string = RGBtoString(thisBlob.color);
 
   console.log(
     thisBlob.id[0].toUpperCase() +
--- lib/mathLib.ts
+++ lib/mathLib.ts
@@ -6,6 +6,77 @@ const randomColor: () => [number, number, number] = () => {
   ];
 };
 
+const RGBtoString: (color: [number, number, number]) => string = (color) => {
+  let i: number;
+  let output: string = "#";
+  for (i = 0; i < 3; i++) {
+    let colorCharacterNumber: number = Math.floor(color[i] / 16);
+
+    switch (colorCharacterNumber) {
+      case 10: {
+        output += "a";
+        break;
+      }
+      case 11: {
+        output += "b";
+        break;
+      }
+      case 12: {
+        output += "c";
+        break;
+      }
+      case 13: {
+        output += "d";
+        break;
+      }
+      case 14: {
+        output += "e";
+        break;
+      }
+      case 15: {
+        output += "f";
+        break;
+      }
+      default: {
+        output += colorCharacterNumber.toString();
+      }
+    }
+
+    colorCharacterNumber = color[i] - colorCharacterNumber * 16;
+
+    switch (colorCharacterNumber) {
+      case 10: {
+        output += "a";
+        break;
+      }
+      case 11: {
+        output += "b";
+        break;
+      }
+      case 12: {
+        output += "c";
+        break;
+      }
+      case 13: {
+        output += "d";
+        break;
+      }
+      case 14: {
+        output += "e";
+        break;
+      }
+      case 15: {
+        output += "f";
+        break;
+      }
+      default: {
+        output += colorCharacterNumber.toString();
+      }
+    }
+  }
+  return output;
+};
+
 const average: (
   a: number,
   b: number,
@@ -19,4 +90,4 @@ const average: (
   }
 };
 
-export { randomColor, average };
+export { randomColor, RGBtoString, average };
